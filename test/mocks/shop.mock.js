cd3a64048e73d313eb1cf62ede42ebee58f50e74
Zeppelin17 <polmonclus@outlook.com>
2024-05-12 14:11:41 +0200

    test: Finish shop testing

--- test/mocks/shop.mock.js
+++ test/mocks/shop.mock.js
@@ -1,2 +1,5 @@
 export const getShopsResponse = [{id: 1, name: "dummy shop 1"}, {id: 2, name: "dummy shop 2"}]
 export const getShopResponse = {id: 1, name: "dummy shop 1"}
+export const putShopResponse = {id: 1, name: "dummy shop modified"}
+export const postShopResponse = {id: 1, name: "dummy shop created"}
+export const deleteShopResponse = 204
--- test/shop.test.js
+++ test/shop.test.js
@@ -1,7 +1,6 @@
 import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest'
-import { getShops, getShop, putShop, postShop, deleteShop, getShops } from '../src/utils'
-import { getShopsResponse, getShopResponse } from './mocks/shop.mock'
-import { number } from 'astro/zod'
+import { getShops, getShop, putShop, postShop, deleteShop } from '../src/utils'
+import { getShopsResponse, getShopResponse, putShopResponse, postShopResponse, deleteShopResponse } from './mocks/shop.mock'
 
 // create spy for global fetch
 const fetchSpy = vi.spyOn(global, "fetch")
@@ -70,6 +69,87 @@ describe("API shop interaction", () => {
         expect(typeof shop.id).toBe("number")
         expect(typeof shop.name).toBe("string")
         expect(shop.id).toBeGreaterThan(0)
+    })
+
+    test("edit shop", async () => {
+        const mockedFetch = () => Promise.resolve({
+            json() {
+                return putShopResponse
+            }
+        })
+
+        fetchSpy.mockImplementation(mockedFetch)
+
+        const id = 1
+        const newName = putShopResponse.name
+        const shop = await putShop(id, newName)
+
+        expect(fetch).toHaveBeenCalledWith(
+            `${BASE_API_URL}${API_VERSION}/shops/${id}/`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({name: putShopResponse.name})
+            }
+        )
+
+        expect(shop).toEqual(putShopResponse)
+        expect(typeof shop.id).toBe("number")
+        expect(typeof shop.name).toBe("string")
+        expect(shop.id).toBeGreaterThan(0)
+    })
+
+    test("create shop", async () => {
+        const mockedFetch = () => Promise.resolve({
+            json() {
+                return postShopResponse
+            }
+        })
+
+        fetchSpy.mockImplementation(mockedFetch)
+
+        const newName = postShopResponse.name
+        const shop = await postShop(newName)
+
+        expect(fetch).toHaveBeenCalledWith(
+            `${BASE_API_URL}${API_VERSION}/shops/`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({name: postShopResponse.name})
+            }
+        )
+
+        expect(shop).toEqual(postShopResponse)
+        expect(typeof shop.id).toBe("number")
+        expect(typeof shop.name).toBe("string")
+        expect(shop.id).toBeGreaterThan(0)
+    })
+
+    test("delete shop", async () => {
+        const mockedFetch = () => Promise.resolve(deleteShopResponse)
+
+        fetchSpy.mockImplementation(mockedFetch)
+
+        const id = 1
+        const token = "token"
+        const response = await deleteShop(token, id)
+
+        expect(fetch).toHaveBeenCalledWith(
+            `${BASE_API_URL}${API_VERSION}/shops/${id}/`,
+            {
+                method: "DELETE",
+                headers: {
+                    "Authorization": `Token ${token}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+
     })
 
 
